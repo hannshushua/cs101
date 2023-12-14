d9ef88d54066466c225d8b237cb835e83bd95ceb
hannshushua <151006883+hannshushua@users.noreply.github.com>
2023-12-14 22:56:09 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -3,52 +3,21 @@
 int main(){
     
     int n = 12345;
-    int p1 , p2 , p3 , p4;
-    p1 = p2 = p3 = p4 = 0;
+    int p1 , p4;
+    p1 = p4 = 0;
     int ans = n ;
-    int mid = n ;
     
     for(int i = 1 ; i <= 4 ; i++){
         if(i == 1){
-            p1 = mid % 10;
-        }
-        if(i == 2){
-            p2 = mid % 10;
-        }
-        if(i == 3)
-        {
-            p3 = mid % 10;
+            p1 = n % 10;
         }
         if(i == 4){
-            p4 = mid % 10;
+            p4 = n % 10;
         }
-        mid /= 10;
+        n /= 10;
     }
         ans += 999 * (p1 - p4);
-
-    if(mid>=1000)
-    {
-        if(n % 10 == 0){
-            if(p1 == 0 && p2 == 0 && p3 == 0){
-            printf("000%d",ans);
-            }else if (p1 == 0 && p3 == 0){
-            printf("00%d",ans);
-            }else{
-            printf("0%d",ans);
-            }
-        }else{
-            printf("%d",ans);
-        }
-    
-    }else{
-        p1 = n % 10 ;
-        ans += 999 * (p1 - p4);
-        if(p1 == 0 && n >= 100){
-            printf("0%d",ans);
-        }else{
-            printf("00%d",ans);
-        }
-    }
+        printf("%d",ans);
 
     return 0;
 }
