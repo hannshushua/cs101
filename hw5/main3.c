66c4d6dfbfe983fd1b396800318955a32b140238
hannshushua <151006883+hannshushua@users.noreply.github.com>
2023-12-14 18:13:57 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -5,7 +5,7 @@ int main(){
     for (int i = 1 , j = 1 ; i <= 9 ; j++){
         int ans = i * j;
         printf("%d x %d = %2d|", i, j, ans);
-        if(j%9==0){
+        if(j == 9){
             j = 0;
             i++;
             printf("\n");
