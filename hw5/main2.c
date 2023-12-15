084b347bee9482022b1ef747a188b76ba50daed7
hannshushua <151006883+hannshushua@users.noreply.github.com>
2023-12-15 17:39:57 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -2,20 +2,27 @@
 
 int main(){
     double PI = 0.0;
+    int x = 0;
+    double sum;
 
-    for (int i = 0; i <= 10000000; i++){
-        double sum = 1.0 / (2 * i + 1);
-        if(i%2==0){
-            PI += sum;
-        }else{
+    for (int i = 0; i <= 100000; i++){
+        
+        sum = 1.0 / (2 * i + 1);
+        
+        if(i % 2){
             PI -= sum;
+        }else{
+            PI += sum;
+        }
+    
+        if((int)(PI * 4 * 100000) == 314159){
+                x = i;
+                break;
         }
     }
-    PI *= 4;
-    printf("%.5f", PI);
-
+    
+    printf("%.5lf\n", 4 * PI);
+    printf("%d",x); // x = 136120次
+    
     return 0;
 }
-
-
-
